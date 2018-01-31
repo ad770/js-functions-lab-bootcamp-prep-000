97950699cc221d4b6ca9d3c28306dd339975357c
arye dov eidelman <aryedov.e@gmail.com>
2018-01-31 21:03:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 
-happyHolidaysTo(name){
+function happyHolidaysTo(name){
   return "Happy holidays, ${name}!";
 }
 
-happyHolidayTo(holiday, name){
+function happyHolidayTo(holiday, name){
   return "Happy ${holiday}, ${name}!";
 }
 
-holidayCountdown(holiday, days){
+function holidayCountdown(holiday, days){
   return "It\'s ${days} days until ${holiday}!";
 }
